08f5eea8de0287c5d17049c23682e563ca103205
kota883 <78450427+kota883@users.noreply.github.com>
2021-04-29 21:10:09 +0900

    修正　バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   end
 
   with_options presence: true do
-    validates :item_image, :name, :detail
+    validates :item_image, :name, :detail, :price
   end
 
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
