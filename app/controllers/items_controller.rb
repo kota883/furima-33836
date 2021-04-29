2769d9362d4fcabb98e611421c3e151300b92e05
kota883 <78450427+kota883@users.noreply.github.com>
2021-04-29 16:59:05 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  
+
   def index
   end
 
@@ -12,13 +12,14 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
-    elsif
-      render :new
+    elsif render :new
     end
   end
 
   private
+
   def item_params
-    params.require(:item).permit(:name, :detail, :category_id, :item_status_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, :price, :item_image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :detail, :category_id, :item_status_id, :shipping_fee_id, :prefecture_id,
+                                 :days_to_ship_id, :price, :item_image).merge(user_id: current_user.id)
   end
 end
